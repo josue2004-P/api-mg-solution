e80230be222f900fa2ed243ba05e9de781da8dea
Josue Perez <josuepe03@hotmail.com>
2025-05-03 11:47:35 -0600

    ADD MEJORAS RESPUESTA LOGIN AUTH

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -4,17 +4,21 @@ const jwt = require("jsonwebtoken");
 const validarJWT = async (req, res = response, next) => {
   // x-token headers
   const token = req.header("x-token");
-  
+
   if (!token) {
     return res.status(401).json({
       ok: false,
       msg: "No hay token en la petición",
     });
   }
-
-
-  next();
-
+  try {
+    const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
+    req.uid = uid;
+    req.name = name;
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "Token no válido o expirado" });
+  }
 };
 
 module.exports = {
