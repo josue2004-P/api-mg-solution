762b9250d550ce1f39e79e605d54ec5670ff10b8
Josue Perez <josuepe03@hotmail.com>
2025-08-09 23:11:39 -0600

    update: docker-compose.ml and entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -5,17 +5,15 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 
-# Instala cliente mysql (netcat no es necesario para mysql ping)
 RUN apt-get update && apt-get install -y default-mysql-client
 
 COPY . .
 
 RUN npx prisma generate
 
-# Copia el script y dale permisos
 COPY entrypoint.sh /app/entrypoint.sh
 RUN chmod +x /app/entrypoint.sh
 
 EXPOSE 3000
 
-ENTRYPOINT ["/bin/bash", "/app/entrypoint.sh"]
+ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "🚀 El contenedor se ha iniciado correctamente"
 echo "⏳ Esperando a que MySQL esté disponible en bd_mysql_mvp..."
+
 until mysqladmin ping -h bd_mysql_mvp -P 3306 --silent; do
   echo "❌ MySQL no disponible aún, esperando..."
   sleep 2
@@ -9,5 +10,11 @@ done
 echo "✅ MySQL disponible. Ejecutando migraciones..."
 npx prisma migrate deploy
 
-echo "🚀 Iniciando la aplicación..."
-exec npm run dev
+# Verifica entorno y ejecuta el comando correcto
+if [ "$NODE_ENV" = "development" ]; then
+  echo "🌱 Entorno de desarrollo detectado. Ejecutando: npm run dev"
+  exec npm run dev
+else
+  echo "🏭 Entorno de producción detectado. Ejecutando: npm run start"
+  exec npm run start
+fi
